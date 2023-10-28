9bd5b9f2e24a5216803785a671d0e0a2922cbddb
Asmin Othuru <118998285+AsminOthuru@users.noreply.github.com>
2023-10-28 10:49:47 +0530

    SimpleWordCounter.java

--- SimpleWordCounter.java
+++ SimpleWordCounter.java
@@ -1,11 +1,13 @@
-package AsminDsaProject;
+package AsminDsaProject; // Package declaration
 
-public interface SimpleWordCounter {
-    void countCharacters(String path);
-    void characterFrequency(String path);
-    void countWords(String path);
-    void wordFrequency(String path);
-    void countLines(String path);
-    void printContent(String path);
+public interface SimpleWordCounter { // Declaring an interface for simple word counting functionalities
 
+    // Method declarations for counting characters, word frequencies, and other operations in a file
+
+    void countCharacters(String path); // Method to count characters in a file
+    void characterFrequency(String path); // Method to determine character frequency in a file
+    void countWords(String path); // Method to count the total number of words in a file
+    void wordFrequency(String path); // Method to determine word frequency in a file
+    void countLines(String path); // Method to count the total number of lines in a file
+    void printContent(String path); // Method to print the content of a file
 }
